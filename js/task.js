79f36c2484f872fcb97aadb509222c64472d54e9
Dmyryi <muzalevskyid333@gmail.com>
2022-04-24 13:52:10 +0300

    .

---
+++ js/task.js
@@ -0,0 +1,30 @@
+const buttonAdd = document.querySelector('[data-action="decrement"]')
+const buttonRem = document.querySelector('[data-action="increment"]')
+const value = document.querySelector("#value")
+
+const Div = document.querySelector('#counter')
+const handleClick = (event) => {
+    
+
+    
+  };
+
+  const handleRemove = () =>{
+      val.removeEventListener
+  }
+  buttonAdd.addEventListener("click",handleClick);
+
+
+buttonRem.removeEventListener("click", handleRemove)
+
+  console.log(Div)
+
+
+
+//   const singleBtn = document.querySelector("#single");
+
+// const handlerClick = () => {
+//   console.log("click event listener callback");
+// };
+
+// singleBtn.addEventListener("click", handlerClick);
